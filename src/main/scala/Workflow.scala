fd40a857f9b2e652a8f35c42cd31ac5293908e65
colin-passiv <colin-passiv@users.noreply.github.com>
2016-11-12 20:58:46 +0000

    add scaladoc and visiblity

--- src/main/scala/Workflow.scala
+++ src/main/scala/Workflow.scala
@@ -21,23 +21,32 @@ object Workflow {
     router:      {def post(stepKey: String): Call; def get(stepKey: String): Call}
   )
 
-  case class WorkflowContext[A](
+  case class WorkflowContext[A] private (
     actionCurrent:  Call,
     actionPrevious: Option[Call],
     stepObject:     Option[A],
     restart:        Call,
-    goto:           String => Call // TODO can we avoid this?
+    goto:           String => Call // TODO can we avoid this? maybe return Option[Call] ?
   )
 
+  /** A single step in a Workflow.
+   *  @constructor create a new Step with get, post and optionally ws.
+   *  @param get defines the steps initial loading. If returns None, then the initial request will go straight to {{{post}}}.
+   *  @param post defines the steps processing. This may involve form validation, which may return a Result, if failed, or
+   *              a value to be stored in the session, and made accessible to the following steps.
+   *  @param ws optionally, steps may support WebSockets. The websockets will have access
+   *            to the context, and any step data just as get and post. However it is expected
+   *            that a post will be made to advance the workflow.
+   */
  // TODO both get and ws are optional - conform the type?
   case class Step[A](
-    get:  WorkflowContext[A] => Request[Any]  => Future[Option[Result]],
-    post: WorkflowContext[A] => Request[Any]  => Future[Either[Result, A]],
-    ws:   Option[WorkflowContext[A] => RequestHeader => WebSocket[String, String]] = None
+    get:  WorkflowContext[A] => Request[Any] => Future[Option[Result]],
+    post: WorkflowContext[A] => Request[Any] => Future[Either[Result, A]],
+    ws:   Option[WorkflowContext[A] => RequestHeader => WebSocket[String, String]] = None // TODO support any input, output (not just String) if a reader and writer are provided.
   )
 
   sealed trait WorkflowSyntax[+Next]
-  object WorkflowSyntax {
+  private object WorkflowSyntax {
     case class WSStep[A, Next](label: String, step: Step[A], reader: upickle.default.Reader[A], writer: upickle.default.Writer[A], next: A => Next) extends WorkflowSyntax[Next]
   }
 
@@ -47,6 +56,7 @@ object Workflow {
     }
   }
 
+  /** Defines a sequence of steps to be executed */
   type Workflow[A] = Free[WorkflowSyntax, A]
 
   object Workflow extends cats.Monad[Workflow] {
@@ -61,11 +71,18 @@ object Workflow {
       Free.pure(x)
   }
 
+  /** Wraps a Step as a Workflow so can be composed in a Workflow
+   *  @param label the name of the step. Is used in URLs to identify the current step,
+   *         and as the key to the result when stored - needs to be unique in the workflow
+   *  @param step the step to be included in the workflow
+   *  @param reader defines how to read the step result out of the session
+   *  @param writer defines how to write the step result to the session
+   */
   def step[A](label: String, step: Step[A])(implicit reader: upickle.default.Reader[A], writer: upickle.default.Writer[A]): Workflow[A] = {
     Free.liftF(WorkflowSyntax.WSStep[A,A](label, step, reader, writer, identity))
   }
 
-  def mkWorkflowContext[A](wfc: WorkflowConf[A], label: String, previousLabel: Option[String], optB: Option[A]): WorkflowContext[A] = {
+  private def mkWorkflowContext[A](wfc: WorkflowConf[A], label: String, previousLabel: Option[String], optB: Option[A]): WorkflowContext[A] = {
     val actionCurrent = wfc.router.post(label)
     WorkflowContext(
       actionCurrent  = actionCurrent,
@@ -76,6 +93,13 @@ object Workflow {
     )
   }
 
+  /** Will execute a workflow and return an Action result. The GET request will be
+   *  directed to the indicated stepId.
+   *
+   *  @param wfc the configuration defining the workflow
+   *  @param stepId the current step position. A stepId value of {{{"start"}}} will
+             clear the session and redirect to the first stepId in the workflow.
+   */
   def getWorkflow[A](wfc: WorkflowConf[A], stepId: String)(implicit request: Request[Any]): Future[Result] = {
     logger.warn(s"getWorkflow $stepId")
     if (stepId == "start") {
@@ -89,7 +113,7 @@ object Workflow {
     }
   }
 
-  def doGet[A](wfc: WorkflowConf[A], targetLabel: String, previousLabel: Option[String])(wf: Workflow[A]): Request[Any] => Future[Option[Result]] =
+  private def doGet[A](wfc: WorkflowConf[A], targetLabel: String, previousLabel: Option[String])(wf: Workflow[A]): Request[Any] => Future[Option[Result]] =
     request => wf.fold(
       { a: A => sys.error("doGet: flow finished!") // flow has finished (only will happen if last step has a post)
       },
@@ -107,12 +131,18 @@ object Workflow {
       }
     )
 
+  /** Will execute a workflow and return an Action result. The POST request will be
+   *  directed to the indicated stepId.
+   *
+   *  @param wfc the configuration defining the workflow
+   *  @param stepId the current step position.
+   */
   def postWorkflow[A](wfc: WorkflowConf[A], stepId: String)(implicit request: Request[Any]): Future[Result] = {
     logger.warn(s"postWorkflow $stepId")
     doPost(wfc, stepId, None)(wfc.workflow)(request)
   }
 
-  def doPost[A](wfc: WorkflowConf[A], targetLabel: String, previousLabel: Option[String])(wf: Workflow[A]): Request[Any] => Future[Result] =
+  private def doPost[A](wfc: WorkflowConf[A], targetLabel: String, previousLabel: Option[String])(wf: Workflow[A]): Request[Any] => Future[Result] =
     request => wf.fold(
       { a: A => sys.error("doPost: flow finished!") // flow has finished (only will happen if last step has a post)
       },
@@ -137,7 +167,15 @@ object Workflow {
       }
     )
 
-  type WS[A,B] = scala.concurrent.Future[Either[play.api.mvc.Result,(play.api.libs.iteratee.Enumerator[A], play.api.libs.iteratee.Iteratee[B,Unit]) => Unit]]
+  private type WS[A,B] = scala.concurrent.Future[Either[play.api.mvc.Result,(play.api.libs.iteratee.Enumerator[A], play.api.libs.iteratee.Iteratee[B,Unit]) => Unit]]
+
+  /** Will execute a workflow and return an Action result. The websocket request will be
+   *  directed to the indicated stepId. A Runtime exception will be thrown if the Step does not
+   *  support a websocket request.
+   *
+   *  @param wfc the configuration defining the workflow
+   *  @param stepId the current step position.
+   */
   def wsWorkflow[A](wfc: WorkflowConf[A], currentLabel: String): RequestHeader => WS[String, String] = request => {
     logger.warn(s"wsWorkflow $currentLabel")
     doWs(wfc, currentLabel, None)(wfc.workflow)(request) match {
@@ -145,7 +183,7 @@ object Workflow {
     }
   }
 
-  def doWs[A](wfc: WorkflowConf[A], targetLabel: String, previousLabel: Option[String])(wf: Workflow[A]): RequestHeader => WebSocket[String, String] =
+  private def doWs[A](wfc: WorkflowConf[A], targetLabel: String, previousLabel: Option[String])(wf: Workflow[A]): RequestHeader => WebSocket[String, String] =
     request => wf.fold(
       { a: A => sys.error("doGet: flow finished!") // flow has finished (only will happen if last step has a post)
       },
@@ -162,20 +200,19 @@ object Workflow {
       }
     )
 
-  def dataFor[A](label: String, session: Session)(implicit reader: upickle.default.Reader[A]): A = {
+  private def dataFor[A](label: String, session: Session)(implicit reader: upickle.default.Reader[A]): A =
     session.get(label) match {
       case Some(a) => upickle.default.read[A](a)
       case None    => sys.error(s"invalid state - should have stored result for step $label")
     }
-  }
-  def optDataFor[A](label: String, session: Session)(implicit reader: upickle.default.Reader[A]): Option[A] = {
+
+  private def optDataFor[A](label: String, session: Session)(implicit reader: upickle.default.Reader[A]): Option[A] =
     session.get(label) match {
       case Some(a) => Some(upickle.default.read[A](a))
       case None    => None
     }
-  }
 
-  def nextLabel[A](wf: Workflow[A]) = wf.resume match {
+  private def nextLabel[A](wf: Workflow[A]) = wf.resume match {
     case Xor.Left(ws: WorkflowSyntax.WSStep[_, _]) => ws.label
     case err                                       => sys.error(s"no next label: $err")
   }
